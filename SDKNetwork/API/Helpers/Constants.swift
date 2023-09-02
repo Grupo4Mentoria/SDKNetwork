6689bb075821f73503cb95a29358d7b87863bc32
Alex Sandro <alexsandrojr.303@gmail.com>
2023-09-02 14:17:51 -0300

    feat: create contants to use in providers

---
+++ SDKNetwork/API/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  SDKNetwork
+//
+//  Created by Alex Sandro on 02/09/23.
+//
+
+import Foundation
+
+public struct Constants {
+    public static let defaultToken = "CodandoComMoa"
+    public static let baseURL = "http://mentoria.codandocommoa.com.br"
+}
+
