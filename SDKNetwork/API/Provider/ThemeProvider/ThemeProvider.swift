60f255655b4fe86a8e1d4866aba770eb6a3d5eba
Alex Sandro <alexsandrojr.303@gmail.com>
2023-09-03 11:38:03 -0300

    feat: refactor code to best reading

--- SDKNetwork/API/Provider/ThemeProvider/ThemeProvider.swift
+++ SDKNetwork/API/Provider/ThemeProvider/ThemeProvider.swift
@@ -12,18 +12,18 @@ public class ThemeProvider {
         
     }
     
-    let httpRequest = NetworkService()
+    let networkService = NetworkService()
     
     func getThemeList(completion: @escaping (Result<[ThemeModel], NetworkError>) -> Void) {
-        let EndPoint = Endpoint(url: "\(Constants.baseURL)/Api/Theme/GetListaTheme?uIdFirebase=\(Constants.defaultToken)", method: .get)
+        let EndPoint = Endpoint(url: "\(Constants.baseURL)/\(Constants.themeAPI)/GetListaTheme?uIdFirebase=\(Constants.defaultToken)")
         
-        return httpRequest.request(with: EndPoint, decodeType: [ThemeModel].self, completionHandler: completion)
+        return networkService.request(with: EndPoint, decodeType: [ThemeModel].self, completionHandler: completion)
     }
     
     func getThemeById(id: Int, completion: @escaping (Result<ThemeByIdModel, NetworkError>) -> Void) {
-        let EndPoint = Endpoint(url: "\(Constants.baseURL)/Api/Theme/GetThemeById?id=\(id)&uIdFirebase=\(Constants.defaultToken)", method: .get)
+        let EndPoint = Endpoint(url: "\(Constants.baseURL)/\(Constants.themeAPI)/GetThemeById?id=\(id)&uIdFirebase=\(Constants.defaultToken)")
         
-        return httpRequest.request(with: EndPoint, decodeType: ThemeByIdModel.self, completionHandler: completion)
+        return networkService.request(with: EndPoint, decodeType: ThemeByIdModel.self, completionHandler: completion)
     }
     
 }
