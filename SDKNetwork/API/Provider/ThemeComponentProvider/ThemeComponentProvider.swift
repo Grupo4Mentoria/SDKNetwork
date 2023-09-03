8fff29893aadfa80b9e7ddc3b5f6514eaa882bfe
Alex Sandro <alexsandrojr.303@gmail.com>
2023-09-03 11:37:18 -0300

    feat: create theme component provider

---
+++ SDKNetwork/API/Provider/ThemeComponentProvider/ThemeComponentProvider.swift
@@ -0,0 +1,29 @@
+//
+//  ThemeComponentProvider.swift
+//  SDKNetwork
+//
+//  Created by Alex Sandro on 03/09/23.
+//
+
+import Foundation
+
+public class ThemeComponentProvider {
+    public init() {
+        
+    }
+    
+    let networkService = NetworkService()
+    
+    func getThemeComponentList(themeId: Int, completion: @escaping (Result<[ThemeComponentModel], NetworkError>) -> Void) {
+        let EndPoint = Endpoint(url: "\(Constants.baseURL)/\(Constants.themeComponentAPI)/GetListaThemeComponent?themeId=\(themeId)&uIdFirebase=\(Constants.defaultToken)")
+        
+        return networkService.request(with: EndPoint, decodeType: [ThemeComponentModel].self, completionHandler: completion)
+    }
+    
+    func getThemeComponentById(id: Int, completion: @escaping (Result<ThemeComponentByIdModel, NetworkError>) -> Void) {
+        let EndPoint = Endpoint(url: "\(Constants.baseURL)/\(Constants.themeComponentAPI)/GetThemeComponentById?id=\(id)&uIdFirebase=\(Constants.defaultToken)")
+        
+        return networkService.request(with: EndPoint, decodeType: ThemeComponentByIdModel.self, completionHandler: completion)
+    }
+    
+}
